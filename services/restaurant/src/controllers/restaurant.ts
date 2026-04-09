a444d822e1f4aba284e757d958b7edcdae7ead4b
Shivam Kapoor <kapoorshivam7178@gmail.com>
2026-04-09 14:46:47 +0530

    feat: geolocation APIs added

--- services/restaurant/src/controllers/restaurant.ts
+++ services/restaurant/src/controllers/restaurant.ts
@@ -5,6 +5,7 @@ import getBuffer from "../config/datauri.js";
 import axios from 'axios';
 import Restaurant from "../models/restaurant.js";
 import jwt from 'jsonwebtoken';
+import { isPromise } from "util/types";
 
 export const addRestaurant = TryCatch(async (req : AuthenticatedRequest, res : Response) => {
     const user = req.user;
@@ -167,3 +168,59 @@ export const updateRestaurant = async (req : AuthenticatedRequest, res : Respons
         restaurant, 
     })
 };
+
+export const getNearbyRestaurant = TryCatch( async (req : AuthenticatedRequest, res : Response) => {
+    const { latitude, longitude, radius=5000, search = ""} = req.query;
+
+    if(!latitude || !longitude) return res.status(400).json({
+        message : "Latitude and Longitude are required",
+    });
+
+    const query: any = {
+        isVerified : true, 
+    }
+
+    if(search && typeof search === "string"){
+        query.name = {$regex : search, $options: "i"};
+    }
+
+    const restaurants = await Restaurant.aggregate([
+        {
+            $geoNear : {
+                near : {
+                    type : "Point", 
+                    coordinates : [Number(longitude), Number(latitude)],
+                },
+                distanceField: "distance",
+                maxDistance: Number(radius),
+                spherical : true,
+                query,
+            },
+        },
+        {
+            $sort : {
+                isOpen : -1, 
+                distance : 1,
+            },
+        },
+        {
+            $addFields : {
+                // for rounding off 
+                distanceKm : {
+                    $round : [{$divide : ["$distance", 1000]}, 2]
+                }
+            }
+        }
+    ]);
+
+    res.json({
+        success : true, 
+        count : restaurants.length, 
+        restaurants, 
+    })
+});
+
+export const fetchSingleRestaurant = TryCatch( async (req : AuthenticatedRequest, res : Response) => {
+    const restaurant = await Restaurant.findById(req.params.id);
+    res.json(restaurant);
+});
