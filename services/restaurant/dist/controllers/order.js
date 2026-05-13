d5ba3412b867ab58df7219228dffa23ac2141583
Shivam Kapoor <kapoorshivam7178@gmail.com>
2026-05-13 17:46:54 +0530

    Added Create Order API

---
+++ services/restaurant/dist/controllers/order.js
@@ -0,0 +1,124 @@
+import TryCatch from "../middlewares/trycatch.js";
+import Address from "../models/Address.js";
+import cart from "../models/cart.js";
+import Order from "../models/Order.js";
+import Restaurant from "../models/restaurant.js";
+export const createOrder = TryCatch(async (req, res) => {
+    const DELIVERY_FEE = 30;
+    const user = req.user;
+    if (!user) {
+        return res.status(401).json({
+            message: "Unauthorized",
+        });
+    }
+    const { paymentMethod, addressId, distance } = req.body;
+    if (!addressId) {
+        return res.status(400).json({
+            message: "Address is required",
+        });
+    }
+    if (!paymentMethod) {
+        return res.status(400).json({
+            message: "Payment method is required",
+        });
+    }
+    // optional validation
+    const validPaymentMethods = ["COD", "ONLINE"];
+    if (!validPaymentMethods.includes(paymentMethod)) {
+        return res.status(400).json({
+            message: "Invalid payment method",
+        });
+    }
+    const address = await Address.findOne({
+        _id: addressId,
+        userId: user._id,
+    });
+    if (!address) {
+        return res.status(404).json({
+            message: "Address not found",
+        });
+    }
+    const cartItems = await cart
+        .find({
+        userId: user._id,
+    })
+        .populate("itemId")
+        .populate("restaurantId");
+    if (cartItems.length === 0) {
+        return res.status(400).json({
+            message: "Cart is empty",
+        });
+    }
+    const firstCartItem = cartItems[0];
+    if (!firstCartItem || !firstCartItem.restaurantId) {
+        return res.status(400).json({
+            message: "Invalid cart data",
+        });
+    }
+    const restaurantId = firstCartItem.restaurantId._id;
+    const restaurant = await Restaurant.findById(restaurantId);
+    if (!restaurant) {
+        return res.status(404).json({
+            message: "Restaurant not found",
+        });
+    }
+    if (!restaurant.isOpen) {
+        return res.status(400).json({
+            message: "Restaurant is currently closed",
+        });
+    }
+    let subTotal = 0;
+    const orderItems = cartItems.map((cartItem) => {
+        const item = cartItem.itemId;
+        if (!item) {
+            throw new Error("Invalid cart item");
+        }
+        const itemTotal = cartItem.quantity * item.price;
+        subTotal += itemTotal;
+        return {
+            itemId: item._id.toString(),
+            name: item.name,
+            quantity: cartItem.quantity,
+            price: item.price,
+        };
+    });
+    const deliveryFee = subTotal > 149 ? 0 : DELIVERY_FEE;
+    const platformFee = Number((0.08 * subTotal).toFixed(2));
+    const totalAmount = Number((subTotal + deliveryFee + platformFee).toFixed(2));
+    const expiresAt = new Date(Date.now() + 15 * 60 * 1000);
+    const [longitude, latitude] = address.location.coordinates;
+    const riderAmount = Math.ceil(distance) * 17;
+    const order = await Order.create({
+        userId: user._id.toString(),
+        restaurantId: restaurantId.toString(),
+        restaurantName: restaurant.name,
+        riderId: null,
+        distance,
+        riderAmount,
+        items: orderItems,
+        subTotal,
+        deliveryFee,
+        platformFee,
+        totalAmount,
+        addressId: address._id.toString(),
+        deliveryAddress: {
+            formattedAddress: address.formattedAddress,
+            mobile: address.mobile,
+            latitude,
+            longitude,
+        },
+        paymentMethod,
+        paymentStatus: "pending",
+        status: "placed",
+        expiresAt,
+    });
+    await cart.deleteMany({
+        userId: user._id,
+    });
+    return res.status(201).json({
+        message: "Order created successfully",
+        orderId: order._id.toString(),
+        totalAmount,
+        order,
+    });
+});
